3d024f77aadbe60d73bff43bc952565f3c35d1ed
+ <+>
2023-05-20 03:50:35 +0300

    Done with Assignment

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+conrad = Person("conrad")
+print(conrad.name)
+
+alice = Person("alice")
+print(alice.name)
